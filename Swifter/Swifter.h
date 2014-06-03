e5e87b8a8cc343dcfe14bae13aa021aaf493cb5c
adamkaplan <adamkaplan@yahoo-inc.com>
2014-06-03 17:17:04 -0400

    Add initial Xcode project

---
+++ Swifter/Swifter.h
@@ -0,0 +1,19 @@
+//
+//  Swifter.h
+//  Swifter
+//
+//  Created by Adam Kaplan on 6/3/14.
+//  Copyright (c) 2014 Yahoo!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swifter.
+FOUNDATION_EXPORT double SwifterVersionNumber;
+
+//! Project version string for Swifter.
+FOUNDATION_EXPORT const unsigned char SwifterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swifter/PublicHeader.h>
+
+
