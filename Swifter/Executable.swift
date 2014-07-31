15f7c7033b6fbb6485af828ffcc7df22f6586bc3
Daniel Hanggi <dhanggi@yahoo-inc.com>
2014-07-31 17:31:56 -0400

    Removed unused OnceExecutable.

--- Swifter/Executable.swift
+++ Swifter/Executable.swift
@@ -8,71 +8,26 @@
 
 import Foundation
 
-/* An Executable is a wrapped computation (Task) and thread on which to be executed. */
+/* An Executable is a wrapped action (Task) and queue on which to be executed. */
 public class Executable<T> {
     
-    // TODO: change Task to T -> ()
-    typealias Task = ((Try<T>) -> ())
+    typealias Task = T -> ()
     
+    let queue: NSOperationQueue
     let task: Task
-    let thread: NSOperationQueue
-    private var value: Try<T>!
 
-    /* Creates an Executable to run a Task on an NSOperationQueue, optionally
-     * triggered by a CallbackNotification sent from `observed`. */
-    init(task: Task, thread: NSOperationQueue) {
+    init(queue: NSOperationQueue, task: Task) {
+        self.queue = queue
         self.task = task
-        self.thread = thread
     }
     
     deinit {
         DLog(.Executable, "Deinitializing Executable")
     }
     
-    /* Executes the Executable by running its Task on the NSOperationQueue with `value`. */
-    public func executeWithValue(value: Try<T>) -> () {
-        Log(.Executable, "Executing with \(value) on \(self.thread)")
-        self.value = value
-        self.thread.addOperationWithBlock { _ = self.task(self.value) }
-    }
-    
-}
-
-/* A OnlyExecutableOnceException indicates that the Executable was attempted to 
- * be executed more than once. */
-public class OnlyExecutableOnceException: NSException {
-    
-    init() {
-        super.init(name: "OnlyExecutableOnceException", reason: nil, userInfo: nil)
-    }
-    
-}
-
-/* A OnceExecutable is an Executable that can only be executed once. */
-class OnceExecutable<T> : Executable<T> {
-    
-    typealias OEOE = OnlyExecutableOnceException
-    
-    /* Creates a OnceExecutable to run a Task on an NSOperationQueue, optionally
-     * triggered by a CallbackNotification sent from `observed`. */
-    init(task: Task, thread: NSOperationQueue) {
-        super.init(task: task, thread: thread)
-    }
-    
-    /* Creates a OnceExecutable with the execution parameters as the Executable. */
-    init(parent: Executable<T>) {
-        super.init(task: parent.task, thread: parent.thread)
-    }
-    
-    /* Executes the Executable by running its Task on the NSOperationQueue with `value`. 
-     * This process can only occur once, and the OnceExecutable will raise a 
-     * OnlyExecutableOnceException if called to execute a second time. */
-    override func executeWithValue(value: Try<T>) -> () {
-        if self.value {
-            OEOE().raise()
-        } else {
-            super.executeWithValue(value)
-        }
+    public func executeWithValue(value: T) -> () {
+        Log(.Executable, "Executing with \(value) on \(self.queue)")
+        self.queue.addOperationWithBlock { _ = self.task(value) }
     }
     
 }
